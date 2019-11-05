ac35faa2f228412e9ceb4ec5ca7225982194455f
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2019-11-04 22:27:44 -0500

    Written by Arianne Rull and Alexa Liaskovski

--- Calculator.java
+++ Calculator.java
@@ -1,74 +1,45 @@
 // This is a finished project written by Arianne Rull
-// Under the code review of Alexa L.
+// Under the code review of Alexa Liaskovski
 // For the purpose of teaching L'Amoreaux Computer Science Club about the syntax of Java
 
 import java.util.Scanner;
-class Calculator {
+class Main {
     public static void main(String[] args) {
-    	
-    Scanner input = new Scanner(System.in);
-    	
+      
+      Scanner input = new Scanner(System.in);
+      
       // Getting user's name
       System.out.print("Enter your name: ");
       String myString = input.next();
       System.out.print("Hello world, " + myString);
 
+      // Getting user's option choice
       System.out.print("\n What would you like to do? (add/subtract/divide/multiply) \n Press 1 for addition \n Press 2 for subtraction \n Press 3 for division \n Press 4 for multiplication \n");
       int myChoice = input.nextInt();
-      System.out.print("You chose" + myChoice + "\n");
+      System.out.print("You chose " + myChoice + "\n");
 
-      // Addition
-      if (myChoice < 2){
-        int a, b, c;
+      // Getting the two numbers for calculation
+      System.out.println("Enter the two integers to calculate: ");
+      Scanner in = new Scanner(System.in);
+      int a, b, c;
+      a = in.nextInt();
+      b = in.nextInt();
 
-        System.out.println("Enter the two integers to calculate their sum: ");
-        Scanner in = new Scanner(System.in);
-     
-        a = in.nextInt();
-        b = in.nextInt();
+      // Calculating
+      if (myChoice == 1){ // it's better to use == in this case since you know exactly what to expect from the user. what you had before worked, but if another engineer was reading your code, this would make it easier to understand what the purpose of this code is.
         c = a + b;
-     
         System.out.println("Sum of the integers = " + c);
-
-      } else if (myChoice < 3){
-        System.out.println(myString + "chose to subtract numbers");
-
-        int d, e, f;
-
-        System.out.println("Enter the two integers to calculate their sum: ");
-        Scanner in = new Scanner(System.in);
-     
-        d = in.nextInt();
-        e = in.nextInt();
-        f = d + e;
-     
-        System.out.println("Difference of the integers = " + f);
-      } else if (myChoice < 4){
-        System.out.println(myString + "chose to divide numbers");
-
-        int g, h, i;
-
-        System.out.println("Enter the two integers to calculate their sum: ");
-        Scanner in = new Scanner(System.in);
-     
-        g = in.nextInt();
-        h = in.nextInt();
-        i = g/h;
-     
-        System.out.println("Quotient of the integers = " + i);
-      } else if (myChoice < 5){
-        System.out.println(myString + "chose to multiply numbers");
-
-        int j, k, l;
-
-        System.out.println("Enter the two integers to calculate their sum: ");
-        Scanner in = new Scanner(System.in);
-     
-        j = in.nextInt();
-        k = in.nextInt();
-        l = j/k;
-     
-        System.out.println("Product of the integers = " + l);
+      } else if (myChoice == 2){
+        c = a - b;
+        System.out.println("Difference of the integers = " + c);
+      } else if (myChoice == 3){
+        c = a/b;
+        System.out.println("Quotient of the integers = " + c);
+      } else if (myChoice == 4){
+        c = a*b;
+        System.out.println("Product of the integers = " + c);
+      } else { // its also good to include edge cases, such as if the user provides invalid input.
+        System.out.println("You made an invalid choice!");
       }
     }
 }
